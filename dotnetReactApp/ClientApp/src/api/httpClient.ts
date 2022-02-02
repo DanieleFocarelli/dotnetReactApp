39c3148a39c93d91394546ee30a4eecc277a613c
Daniele Focarelli <d.focarelli@vetrya.com>
2022-02-02 17:05:03 +0100

    added-form

---
+++ dotnetReactApp/ClientApp/src/api/httpClient.ts
@@ -0,0 +1,12 @@
+﻿
+import axios from "axios";
+
+export const getData = async () => {
+     return await axios.get('api/TodoItems');
+}
+
+export const postData = async (payload: any) => {
+    return await axios.post('api/TodoItems', payload);
+}
+
+
--- dotnetReactApp/ClientApp/src/components/Home.tsx
+++ dotnetReactApp/ClientApp/src/components/Home.tsx
@@ -1,22 +1,68 @@
 import * as React from 'react';
+import * as httpClient from '../api/httpClient';
 
-const Home = () => (
-  <div>
-    <h1>Hello, world!</h1>
-    <p>Welcome to your new single-page application, built with:</p>
-    <ul>
-      <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-      <li><a href='https://facebook.github.io/react/'>React</a> and <a href='https://redux.js.org/'>Redux</a> for client-side code</li>
-      <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-    </ul>
-    <p>To help you get started, we have also set up:</p>
-    <ul>
-      <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-      <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-      <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-    </ul>
-    <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-  </div>
-);
+const Home = () => {
+  const [_data, setData] = React.useState<any>()
+  const [_name, setName] = React.useState('')
+  const [_surname, setSurname] = React.useState('')
+  const [_id, setId] = React.useState(0)
+
+  const handleGetData = async () => {
+    const res = await httpClient.getData()
+    setData(res.data)
+  }
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    let newId = _id + 1
+    setId(newId)
+    const response = await httpClient.postData({
+      id: newId,
+      name: _name,
+      surname: _surname
+    })
+    if (response.status == 201) {
+      alert('bravissimi')
+    } else {
+      alert('somari')
+    }
+  }
+
+  return (
+    <div className='d-flex flex-column'>
+      <button onClick={handleGetData}>Get Data</button>
+      {_data
+        ?
+        _data.map((obj: any) => {
+          return JSON.stringify(obj)
+        })
+        :
+        'nessuno stronzo nel db'
+      }
+      <form className='mt-5' onSubmit={handleSubmit}>
+        <label>
+          Nome:
+          <input
+            name="name"
+            type="text"
+            value={_name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Cognome:
+          <input
+            name="surname"
+            type="text"
+            value={_surname}
+            onChange={(event) => setSurname(event.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
 
 export default Home;
